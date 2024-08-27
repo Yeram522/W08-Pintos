f397590d0f6c5180fc40ff3e6a0bb19fa0285da1
KIMHAEGANG <yujin0124@khu.ac.kr>
2024-08-27 17:40:10 +0000

    FIX: solved test fail

--- threads/synch.c
+++ threads/synch.c
@@ -188,9 +188,9 @@ lock_acquire (struct lock *lock) {
 	ASSERT (!intr_context ());
 	ASSERT (!lock_held_by_current_thread (lock));
 
-	if (lock->holder->priority < thread_current ()->priority) {
-		thread_donate_priority();
-	}
+	// if (lock->holder->priority < thread_current ()->priority) {
+	// 	thread_donate_priority();
+	// }
 	sema_down (&lock->semaphore);
 	thread_recover_priority();
 
